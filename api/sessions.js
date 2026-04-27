61350acfb2722da7abc42935afcc37255d36f546
alexfarnum <alex@afarnum.com>
2026-04-27 14:16:05 -0700

    Update sessions.js

--- api/sessions.js
+++ api/sessions.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch('https://api.playground.try.be/shop/sessions', {
+    const response = await fetch('https://api.try.be/shop/sessions', {
       headers: {
         Authorization: `Bearer ${process.env.TRYBE_API_KEY}`,
         Accept: 'application/json'
