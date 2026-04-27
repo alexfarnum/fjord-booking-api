c9b85b40b2e955b10a72192d8d3483f4868641b7
alexfarnum <alex@afarnum.com>
2026-04-27 14:06:41 -0700

    Create sessions.js

---
+++ api/sessions.js
@@ -0,0 +1,28 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://api.playground.try.be/shop/sessions', {
+      headers: {
+        Authorization: `Bearer ${process.env.TRYBE_API_KEY}`,
+        Accept: 'application/json'
+      }
+    });
+
+    const json = await response.json();
+
+    const sessions = (json.data || []).map(session => ({
+      id: session.id,
+      type: session.session_type_name,
+      startTime: session.start_time,
+      endTime: session.end_time,
+      remainingSpots: session.remaining_capacity,
+      price: session.price,
+      soldOut: session.remaining_capacity <= 0
+    }));
+
+    res.status(200).json({ sessions });
+  } catch (error) {
+    res.status(500).json({
+      error: 'Failed to fetch Trybe sessions'
+    });
+  }
+}
